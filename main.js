399cc22acde1fe6076679866d276acf99ee327fe
Raoul Bock <anthonyraoulbock01@gmail.com>
2025-08-04 08:59:48 +0200

    changes

--- main.js
+++ main.js
@@ -8,26 +8,37 @@ function createWindow() {
   win = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: path.join(__dirname, "public/icon.png"),
     webPreferences: {
       contextIsolation: true,
     },
-    autoHideMenuBar: true, // Optional: hide menu bar until Alt is pressed
+    autoHideMenuBar: true,
   });
 
   win.loadFile("dist/index.html");
+
+  win.on("close", (event) => {
+    if (!app.isQuiting) {
+      event.preventDefault();
+      win.hide();
+    }
+  });
 }
 
 app.whenReady().then(() => {
-  // ✅ Remove default application menu
   Menu.setApplicationMenu(null);
-
   createWindow();
 
-  tray = new Tray(path.join(__dirname, "public/icon.png"));
+  // ✅ Use .ico format here
+  tray = new Tray(path.join(__dirname, "public", "favicon.ico"));
   const contextMenu = Menu.buildFromTemplate([
     { label: "Show App", click: () => win.show() },
-    { label: "Quit", click: () => app.quit() },
+    {
+      label: "Quit",
+      click: () => {
+        app.isQuiting = true;
+        app.quit();
+      },
+    },
   ]);
   tray.setToolTip("Hello World Electron");
   tray.setContextMenu(contextMenu);
