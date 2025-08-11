552737b8b2583729f5a5058228945b18e1b86e43
Raoul Bock <anthonyraoulbock01@gmail.com>
2025-08-11 19:38:48 +0200

    fix icon

--- main.js
+++ main.js
@@ -1,26 +1,49 @@
-// main.js (PulseDesk client)
-const { io } = require("socket.io-client");
+const { app, Tray, Menu } = require("electron");
+const path = require("path");
 const os = require("os");
+const { io } = require("socket.io-client");
 
-const socket = io("http://localhost:4000");
+let tray = null;
+let socket;
 
-const deviceInfo = {
-  hostname: os.hostname(),
-  platform: os.platform(),
-  osType: os.type(),
-  osRelease: os.release(),
-  architecture: os.arch(),
-  cpuCount: os.cpus().length,
-};
+function createTray() {
+  tray = new Tray(path.join(__dirname, "public", "favicon.ico")); // path adjusted
 
-socket.on("connect", () => {
-  socket.emit("register", deviceInfo);
-  console.log("Registered PulseDesk client with server");
+  const contextMenu = Menu.buildFromTemplate([
+    { label: "Quit", click: () => app.quit() },
+  ]);
+
+  tray.setToolTip("PulseDesk Client");
+  tray.setContextMenu(contextMenu);
+}
+
+function startSocket() {
+  socket = io("http://localhost:4000");
+
+  const deviceInfo = {
+    hostname: os.hostname(),
+    platform: os.platform(),
+    osType: os.type(),
+    osRelease: os.release(),
+    architecture: os.arch(),
+    cpuCount: os.cpus().length,
+  };
+
+  socket.on("connect", () => {
+    socket.emit("register", deviceInfo);
+    console.log("Registered PulseDesk client with server");
+  });
+
+  setInterval(() => {
+    if (socket.connected) {
+      socket.emit("heartbeat", { hostname: os.hostname() });
+    }
+  }, 20000);
+}
+
+app.whenReady().then(() => {
+  createTray();
+  startSocket();
+
+  app.on("window-all-closed", (e) => e.preventDefault());
 });
-
-setInterval(() => {
-  if (socket.connected) {
-    // Send hostname as an object with heartbeat
-    socket.emit("heartbeat", { hostname: os.hostname() });
-  }
-}, 20000);
