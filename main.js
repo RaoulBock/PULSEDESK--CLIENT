27257adb5ed9f1394fdb081e686c387b6baf2a5c
RaoulBock <anthonyraoulbock01@gmail.com>
2025-08-14 16:00:28 +0200

    changes

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ function createWindow() {
   win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, "public/speedometer-256x256.png"),
     webPreferences: {
       contextIsolation: false,
       nodeIntegration: true,
