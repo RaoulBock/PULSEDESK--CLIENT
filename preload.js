935fe283b401ba850c8e04a2cca1e12731626d2b
RaoulBock <anthonyraoulbock01@gmail.com>
2025-08-14 15:34:20 +0200

    build changes

--- preload.js
+++ preload.js
@@ -1,7 +1,15 @@
+// const { contextBridge, ipcRenderer } = require("electron");
+
+// contextBridge.exposeInMainWorld("electron", {
+//   ipcRenderer: {
+//     invoke: (channel, ...args) => ipcRenderer.invoke(channel, ...args),
+//   },
+// });
+
 const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld("electron", {
-  ipcRenderer: {
-    invoke: (channel, ...args) => ipcRenderer.invoke(channel, ...args),
-  },
+contextBridge.exposeInMainWorld("ipcRenderer", {
+  send: (channel, data) => ipcRenderer.send(channel, data),
+  on: (channel, callback) =>
+    ipcRenderer.on(channel, (event, ...args) => callback(...args)),
 });
